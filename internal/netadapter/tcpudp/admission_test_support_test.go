2e50f8c2dcb73cc09a9e901f7a4d2579ec48adb7
Li41 <li41qq@gmail.com>
2026-08-15 10:12:55 +0800

    S3-F.18：更新 tcpudp fake runtime ownership seam

--- internal/netadapter/tcpudp/admission_test_support_test.go
+++ internal/netadapter/tcpudp/admission_test_support_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/li41/astrahold-server/internal/characteridentity"
+	"github.com/li41/astrahold-server/internal/protocol"
 	"github.com/li41/astrahold-server/internal/worldruntime"
 )
 
@@ -18,4 +19,31 @@ func (f *fakeRuntime) ReleaseCharacterAdmission(context.Context, worldruntime.Ch
 
 func (f *fakeRuntime) AwaitJoin(_ context.Context, request worldruntime.JoinRequest) error {
 	return f.EnqueueJoin(request)
+}
+
+func (f *fakeRuntime) AwaitJoinOwned(_ context.Context, request worldruntime.JoinRequest) (worldruntime.SessionOwnershipFence, error) {
+	if err := f.EnqueueJoin(request); err != nil {
+		return worldruntime.SessionOwnershipFence{}, err
+	}
+	if request.Session.CharacterIdentity.Assurance != characteridentity.AssuranceTrusted {
+		return worldruntime.SessionOwnershipFence{}, nil
+	}
+	return worldruntime.SessionOwnershipFence{
+		SessionID: request.Session.ID,
+		EntityID: request.Session.EntityID,
+		CharacterID: request.Session.CharacterIdentity.ID,
+		Epoch: 1,
+	}, nil
+}
+
+func (f *fakeRuntime) EnqueueFencedLeave(fence worldruntime.SessionOwnershipFence) error {
+	return f.EnqueueLeave(fence.SessionID)
+}
+
+func (f *fakeRuntime) EnqueueFencedMove(fence worldruntime.SessionOwnershipFence, sequence uint32, input protocol.ClientMoveInput) error {
+	return f.EnqueueMove(fence.SessionID, sequence, input)
+}
+
+func (f *fakeRuntime) EnqueueFencedUseAction(fence worldruntime.SessionOwnershipFence, sequence uint32, action protocol.ClientUseAction) error {
+	return f.EnqueueUseAction(fence.SessionID, sequence, action)
 }
