9128a8cc008d16a668aa10c8ff55f1670d526798
Li41 <li41qq@gmail.com>
2026-08-15 09:43:28 +0800

    S3-F.17：更新 tcpudp admission test runtime

--- internal/netadapter/tcpudp/admission_test_support_test.go
+++ internal/netadapter/tcpudp/admission_test_support_test.go
@@ -2,12 +2,17 @@ package tcpudp
 
 import (
 	"context"
+	"time"
 
 	"github.com/li41/astrahold-server/internal/characteridentity"
 	"github.com/li41/astrahold-server/internal/worldruntime"
 )
 
-func (f *fakeRuntime) AwaitCharacterAdmission(context.Context, characteridentity.Binding) error {
+func (f *fakeRuntime) AwaitCharacterAdmission(_ context.Context, identity characteridentity.Binding) (worldruntime.CharacterAdmissionLease, error) {
+	return worldruntime.CharacterAdmissionLease{CharacterID: identity.ID, Generation: 1, ExpiresAt: time.Now().Add(time.Minute)}, nil
+}
+
+func (f *fakeRuntime) ReleaseCharacterAdmission(context.Context, worldruntime.CharacterAdmissionLease) error {
 	return nil
 }
 
