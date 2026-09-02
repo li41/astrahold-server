bbfa1d04dcd3412583338a4d39faa0c56a59c89a
Li41 <li41qq@gmail.com>
2026-09-02 21:49:31 +0800

    feat: fence equipment intents by session ownership

---
+++ internal/worldruntime/equipment_ownership.go
@@ -0,0 +1,20 @@
+package worldruntime
+
+import (
+	"errors"
+
+	"github.com/li41/astrahold-server/internal/protocol"
+)
+
+func (r *Runtime) EnqueueFencedEquipmentCommand(fence SessionOwnershipFence, sequence uint32, equipment protocol.ClientEquipmentCommand) error {
+	if !fence.Valid() {
+		return ErrCharacterOwnershipFenceInvalid
+	}
+	if sequence == 0 {
+		return errors.New("worldruntime: invalid equipment intent")
+	}
+	if err := validateEquipmentIntent(equipment); err != nil {
+		return err
+	}
+	return r.queue.tryPush(equipmentCommand{sessionID: fence.SessionID, sequence: sequence, equipment: equipment, ownership: fence})
+}
