be964de056b40422587cd3ac11a12c87607c17a9
Li41 <li41qq@gmail.com>
2026-09-03 11:07:57 +0800

    style: format NPC protocol definitions

--- internal/protocol/npc.go
+++ internal/protocol/npc.go
@@ -4,7 +4,7 @@ import "github.com/li41/astrahold-server/internal/world"
 
 const (
 	MessageClientInteractNPC MessageType = 5
-	MessageNPCInteraction   MessageType = 112
+	MessageNPCInteraction    MessageType = 112
 )
 
 // ClientInteractNPC is intent only. The Server re-resolves the target entity and validates
