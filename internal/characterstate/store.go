7b798afefd57c806a11ef3506a3a7a3450da1436
Li41 <li41qq@gmail.com>
2026-09-03 19:57:51 +0800

    fix: use platform-aware character state directory sync

--- internal/characterstate/store.go
+++ internal/characterstate/store.go
@@ -220,11 +220,7 @@ func (s *Store) writeLocked(record Record) error {
 	if err := tmp.Sync(); err != nil { cleanup(); return err }
 	if err := tmp.Close(); err != nil { _ = os.Remove(tmpName); return err }
 	if err := os.Rename(tmpName, s.recordPath(record.CharacterID)); err != nil { _ = os.Remove(tmpName); return err }
-	directory, err := os.Open(s.root)
-	if err != nil { return err }
-	syncErr := directory.Sync(); closeErr := directory.Close()
-	if syncErr != nil { return syncErr }
-	return closeErr
+	return syncDirectory(s.root)
 }
 
 func (s *Store) recordPath(id characteridentity.ID) string {
