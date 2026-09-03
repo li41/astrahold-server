9126dc69b1d868853a64219b2d45ea28139cb316
Li41 <li41qq@gmail.com>
2026-09-03 19:57:11 +0800

    fix: make character state directory sync platform-aware

---
+++ internal/characterstate/syncdir_other.go
@@ -0,0 +1,18 @@
+//go:build !windows
+
+package characterstate
+
+import "os"
+
+func syncDirectory(path string) error {
+	directory, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	syncErr := directory.Sync()
+	closeErr := directory.Close()
+	if syncErr != nil {
+		return syncErr
+	}
+	return closeErr
+}
