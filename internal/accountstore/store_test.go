76053396ff4e6fdb9594deb2650a6ad66ae12018
Li41 <li41qq@gmail.com>
2026-09-03 19:02:19 +0800

    test: make accountstore mode assertion platform-aware

--- internal/accountstore/store_test.go
+++ internal/accountstore/store_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 )
 
@@ -17,7 +18,7 @@ func TestStoreSaveLoadAndRevisionCAS(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if info.Mode().Perm() != 0o600 {
+	if runtime.GOOS != "windows" && info.Mode().Perm() != 0o600 {
 		t.Fatalf("mode=%o want=600", info.Mode().Perm())
 	}
 	loaded, err := Load(path)
