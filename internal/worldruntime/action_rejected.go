d5862607b7292f1171947316d61bd5c977cb879d
Li41 <li41qq@gmail.com>
2026-09-11 16:18:44 +0800

    refactor(worldruntime): retire wrong-class action rejection

--- internal/worldruntime/action_rejected.go
+++ internal/worldruntime/action_rejected.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/li41/astrahold-server/internal/character"
 	"github.com/li41/astrahold-server/internal/combat"
-	"github.com/li41/astrahold-server/internal/legacyclassgate"
 	"github.com/li41/astrahold-server/internal/protocol"
 	"github.com/li41/astrahold-server/internal/session"
 	"github.com/li41/astrahold-server/internal/siege"
@@ -69,8 +68,6 @@ func actionRejectionReason(err error) protocol.ActionRejectionReason {
 		return protocol.ActionRejectionCooldown
 	case errors.Is(err, character.ErrInsufficientResource):
 		return protocol.ActionRejectionInsufficientResource
-	case errors.Is(err, legacyclassgate.ErrWrongClass):
-		return protocol.ActionRejectionWrongClass
 	case errors.Is(err, ErrEntityOutOfRange), errors.Is(err, ErrPointOutOfRange), errors.Is(err, siege.ErrGateOutOfRange):
 		return protocol.ActionRejectionOutOfRange
 	case errors.Is(err, ErrEntityWrongLayer), errors.Is(err, siege.ErrGateWrongLayer):
