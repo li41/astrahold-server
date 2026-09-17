dda7a2cbae441651133569ed85bf9d5cc9e6b42b
Li41 <li41qq@gmail.com>
2026-09-17 15:35:30 +0800

    ammunition: align bow regression expectations

--- internal/equipmentcatalog/weapon_progression_test.go
+++ internal/equipmentcatalog/weapon_progression_test.go
@@ -33,7 +33,7 @@ func TestDefaultCatalogLocksThreeTierWeaponProgression(t *testing.T) {
 		{WeaponTypeKnuckles, 4, MaterialSteel, MaterialStarsteel, 2, DamageRange{6,9}, DamageRange{6,8}, DamageRange{7,12}, DamageRange{7,10}, 0, 0},
 		{WeaponTypeClaw, 5, MaterialSteel, MaterialStarsteel, 1, DamageRange{7,11}, DamageRange{7,10}, DamageRange{9,14}, DamageRange{9,13}, 0, 0},
 		{WeaponTypeDualBlades, 8, MaterialSteel, MaterialStarsteel, 0, DamageRange{8,12}, DamageRange{7,10}, DamageRange{11,15}, DamageRange{9,13}, 0, 0},
-		{WeaponTypeBow, 4, MaterialReinforcedWood, MaterialStarwood, 1, DamageRange{3,4}, DamageRange{3,4}, DamageRange{4,5}, DamageRange{4,5}, 0, 0},
+		{WeaponTypeBow, 4, MaterialReinforcedWood, MaterialStarwood, 1, DamageRange{5,7}, DamageRange{5,7}, DamageRange{8,12}, DamageRange{8,12}, 0, 0},
 		{WeaponTypeCrossbow, 8, MaterialSteel, MaterialStarsteel, 2, DamageRange{17,21}, DamageRange{17,21}, DamageRange{22,27}, DamageRange{22,27}, 0, 0},
 		{WeaponTypeSling, 2, MaterialReinforcedLeather, MaterialStarhide, 2, DamageRange{10,13}, DamageRange{10,13}, DamageRange{13,17}, DamageRange{13,17}, 0, 0},
 		{WeaponTypeStaff, 5, MaterialRunewood, MaterialStarwood, 0, DamageRange{7,10}, DamageRange{7,10}, DamageRange{9,13}, DamageRange{9,13}, 0, 0},
